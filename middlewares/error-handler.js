7b9287368028b75e9d9272463f47a172ef01c98c
amilpa <amilpa2020@gmail.com>
2023-04-18 23:12:22 +0530

    setup error-handler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,4 +1,12 @@
 
 const handleError = (err,req,res,next) => {
-  return res.status(500).json({message : err.message})
+  if (err.message == 'Item not found') {
+    return res.status(404).json({ message : err.message })
+  }
+  else
+  {
+    return res.status(500).json({message : err.message})
+  }
 }
+
+module.exports = handleError
